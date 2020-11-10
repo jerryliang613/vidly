f2c501adbb0c95875e5c45b680017ae2ce2c6fc0
Jerry <jerry.liang613@gmail.com>
2020-11-11 10:21:26 +1100

    Updates

---
+++ precache-manifest.83da5dd295ea6b5626928e7b1369e86c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bfe684e3a05606b986b11f840a698b9",
+    "url": "/vidly/index.html"
+  },
+  {
+    "revision": "8dda2d17dc64181eeb87",
+    "url": "/vidly/static/css/2.efd6b4be.chunk.css"
+  },
+  {
+    "revision": "2de4a2a8edcfa1f11276",
+    "url": "/vidly/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8dda2d17dc64181eeb87",
+    "url": "/vidly/static/js/2.2f238b40.chunk.js"
+  },
+  {
+    "revision": "c560b744118e776c5a9fe14e0546e12a",
+    "url": "/vidly/static/js/2.2f238b40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2de4a2a8edcfa1f11276",
+    "url": "/vidly/static/js/main.b930594b.chunk.js"
+  },
+  {
+    "revision": "cb388922157c702ba0d5",
+    "url": "/vidly/static/js/runtime-main.f8720289.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/vidly/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/vidly/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/vidly/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/vidly/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/vidly/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
